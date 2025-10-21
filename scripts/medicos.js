62f7e2c10f007b574467729a9390506873c70b6c
Marco Cippitelli <marco.email.it@gmail.com>
2025-10-21 06:01:22 +0000

    Implementacion provisoria de Admin Dashboard  - Markup  - Estilos  - Persistencia de datos en localStorage

---
+++ scripts/medicos.js
@@ -0,0 +1,34 @@
+export const medicosIniciales = [
+    {
+        apellidos: "Gómez",
+        nombres: "Juan",
+        dni: "12345678",
+        telefono: "555-1234",
+        especialidad: "Cardiología",
+        matricula: "C-001"
+    },
+    {
+        apellidos: "Pérez",
+        nombres: "Ana",
+        dni: "87654321",
+        telefono: "555-5678",
+        especialidad: "Pediatría",
+        matricula: "P-002"
+    },
+    {
+        apellidos: "López",
+        nombres: "Carlos",
+        dni: "11223344",
+        telefono: "555-9012",
+        especialidad: "Dermatología",
+        matricula: "D-003"
+    },
+  {
+        apellidos: "Siller",
+        nombres: "Carla",
+        dni: "112565484",
+        telefono: "555-90959612",
+        especialidad: "Dermatología",
+        matricula: "D-003"
+    },
+];
